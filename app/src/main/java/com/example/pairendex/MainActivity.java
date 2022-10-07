03c2e2b278fd7c82ae297bb70ab2e7f15322500c
GN4k <2021bcs041@sggs.ac.in>
2022-10-08 01:05:00 +0530

    Initial commit

---
+++ app/src/main/java/com/example/pairendex/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.pairendex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void gotoplaypage(View view){
+        Intent intent = new Intent(MainActivity.this,PlayOption.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/pairendex/PlayOption.java
@@ -0,0 +1,20 @@
+package com.example.pairendex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class PlayOption extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_option);
+    }
+    public void gotomainmenu(View view){
+        Intent intent = new Intent(PlayOption.this,MainActivity.class);
+        startActivity(intent);
+    }
+}
